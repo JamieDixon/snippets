6a2b2d9c2034bf27ac60ce2ea356cf07b7e0ae8a
Jamie Dixon <jamie@jamie-dixon.co.uk>
2019-02-05 11:00:51 +0000

    Add some comments etc to map-semigroup.js

--- src/map-semigroup.js
+++ src/map-semigroup.js
@@ -1,25 +1,35 @@
+// Purpose: To explore a way that we might combine 2 user profiles.
+// Let's say a single user has created 2 accounts and wishes to merge them
+
 const inspect = Symbol.for('nodejs.util.inspect.custom');
 
+// First semi-group - Pick the first First in the merge and disgard everything else.
 const First = x => ({
   x,
   concat: x2 => First(x),
   [inspect]: () => `First(${x})`,
 });
 
+// Example First
+const f = First('Hello').concat(First('Foo'));
+console.log(f); // First('Hello')
+
+// Sum semi-group - return the sum of each Sum semi-group
 const Sum = x => ({
   x,
   concat: ({x: y}) => Sum(x + y),
   [inspect]: () => `Sum(${x})`,
 });
 
-const f = First('Hello').concat(First('Foo'));
-console.log(f);
-
+// Example Sum
 const s = Sum(1)
   .concat(Sum(2))
   .concat(Sum(3));
-console.log(s);
+console.log(s); // Sum(6)
 
+// Naive Map implementation
+// concat function will attempt to concat each property.
+// if a property isn't a semi-group, it will be converted to a First semi-group
 const Map = x => ({
   x,
   map: f => Map(f(x)),
@@ -33,6 +43,7 @@ const Map = x => ({
         {},
       ),
     ),
+  // I know, recursive inspect would be nice ¯\_(ツ)_/¯
   [inspect]: () => `Map(${JSON.stringify(x)})`,
 });
 
@@ -50,4 +61,4 @@ const profileB = Map({
 });
 
 // We assert that if all the elements of a category are semi-groups, the category itself is a semi-group
-console.log(profileA.concat(profileB));
+console.log(profileA.concat(profileB)); // Map({"name":{"x":"Jamie"},"rep":{"x":16},"age":{"x":34},"interests":["Coding","functional programming","Physical combat","Dancing"]})
